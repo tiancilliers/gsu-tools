6f9e6ee178fc2fb8800e2c63e04657706d2acfde
Tian Cilliers <tiancilliers@gmail.com>
2024-03-24 19:29:11 +0200

    Initial config and microcontroller drivers

---
+++ gsuconfig.py
@@ -0,0 +1,84 @@
+from enum import Enum
+from dataclasses import dataclass
+from rich.console import Console
+console = Console()
+tobin = lambda list: '[' + ' '.join([f'{i:08b}' for i in list]) + ']'
+
+@dataclass
+class PinConfig:
+    location: tuple
+    opendrain: bool
+    inverted: bool
+
+class EPSConfig(Enum):
+    CAN1 = PinConfig((0, 0), False, True)
+    RS4851 = PinConfig((0, 1), True, False)
+    I2C1 = PinConfig((0, 2), False, False)
+    I2C1_PU = PinConfig((0, 3), False, True)
+    UART1 = PinConfig((0, 4), False, True)
+    UART1_REV = PinConfig((0, 5), False, False)
+    SPI1 = PinConfig((0, 6), False, True)
+    SPI1_SHIFT = PinConfig((0, 7), False, False)
+    REG_3V3_EF = PinConfig((1, 0), True, False)
+    REG_5V_EF = PinConfig((1, 1), True, False)
+    REG_CH1_EF = PinConfig((1, 2), True, False)
+    REG_CH2_EF = PinConfig((1, 3), True, False)
+    RAW_PIN_BUS = PinConfig((1, 4), False, False)
+    RAW_BAT_BUS = PinConfig((1, 5), False, False)
+    VT3V3 = PinConfig((2, 0), False, True)
+    VT5V = PinConfig((2, 1), False, True)
+    VSYS = PinConfig((2, 2), False, True)
+    GPIO = PinConfig((2, 3), False, True)
+
+class OBCConfig(Enum):
+    CAN1 = PinConfig((0, 0), False, True)
+    # TODO: add other pins
+
+class MISCConfig(Enum):
+    CAN1 = PinConfig((0, 0), False, True)
+    # TODO: add other pins
+
+PCAL_IOCR = 0x70
+PCAL_OUT = 0x04
+PCAL_CFG = 0x0C
+PCAL_ADDRESS = {
+    EPSConfig: 0b00100001,
+    OBCConfig: 0b00100000,
+    MISCConfig: 0b00100010
+}
+
+CY8C_ADDRESSES = {
+    # TODO: add CY8C addresses
+}
+
+class GSUConfig:
+    def __init__(self, bus, device: Enum):
+        self.bus = bus
+
+        if not device in [EPSConfig, OBCConfig, MISCConfig]:
+            raise Exception("Invalid device enum provided. Must be EPS, OBC, or MISC.")
+        self.device = device
+        
+        self.opendrain = [0 for i in range(3)]
+        self.state = [0 for i in range(3)]
+        for pin in self.device:
+            self.opendrain[pin.value.location[0]] |= (1 << pin.value.location[1]) if pin.value.opendrain else 0
+            self.state[pin.value.location[0]] |= (1 << pin.value.location[1]) if pin.value.inverted else 0
+        
+        self.bus_write(PCAL_ADDRESS[self.device], PCAL_IOCR, self.opendrain)
+        self.bus_write(PCAL_ADDRESS[self.device], PCAL_OUT, self.state)
+        self.bus_write(PCAL_ADDRESS[self.device], PCAL_CFG, [0 for i in range(3)])
+
+    def bus_write(self, address, register, data, log=True):
+        self.bus.write_i2c_block_data(address, register, data)
+        if log:
+            console.log(f"I2C {address:08b} >> {register:02X} = {tobin(data)}")
+
+    def set_pin(self, pin, value):
+        if type(pin) != self.device:
+            raise Exception("Invalid pin enum provided. Must be of the same type as the device.")
+        pin = pin.value
+        
+        self.state[pin.location[0]] &= ~(1 << pin.location[1])
+        self.state[pin.location[0]] |= (1 << pin.location[1]) if value ^ pin.inverted else 0
+        self.bus_write(PCAL_ADDRESS[self.device], PCAL_OUT, self.state)
---
+++ gsumicro.py
@@ -0,0 +1,99 @@
+from dataclasses import dataclass
+import RPi.GPIO as gpio
+import time
+from functools import reduce
+from rich.console import Console
+console = Console()
+tohex = lambda list: '[' + ' '.join([f'{i:02X}' for i in list]) + ']'
+
+@dataclass
+class Microcontroller:
+    nrst: int
+    boot: int
+    nss: int
+
+EPSMicro = Microcontroller(nrst=38, boot=40, nss=35)
+OBCMicro = Microcontroller(nrst=11, boot=12, nss=36)
+MISC1Micro = Microcontroller(nrst=16, boot=18, nss=15)
+MISC2Micro = Microcontroller(nrst=37, boot=13, nss=22)
+
+class GSUMicro:
+    def __init__(self, micro, bus):
+        self.micro = micro
+        self.bus = bus
+
+        gpio.setmode(gpio.BOARD)
+        gpio.setup(self.micro.nrst, gpio.OUT, initial=gpio.HIGH)
+        gpio.setup(self.micro.boot, gpio.OUT, initial=gpio.LOW)
+        gpio.setup(self.micro.nss, gpio.OUT, initial=gpio.HIGH)
+    
+    def bus_xfer(self, data, log=True):
+        ret = self.bus.xfer(data)
+        if log:
+            console.log(f"SPI >> {tohex(data)} << {tohex(ret)}")
+        return ret
+    
+    def gpio_output(self, pin, state, log=True):
+        gpio.output(pin, gpio.HIGH if state == 1 else gpio.LOW)
+        if log:
+            console.log(f"GPIO {pin} = {state}")
+
+    def reset(self):
+        console.log("Resetting microcontroller...")
+        self.gpio_output(self.micro.nrst, 0)
+        time.sleep(0.1)
+        self.gpio_output(self.micro.nrst, 1)
+        time.sleep(0.1)
+    
+    def reset_bootldr(self):
+        console.log("Entering bootloader...")
+        self.gpio_output(self.micro.boot, 1)
+        time.sleep(0.1)
+        self.reset()
+        self.gpio_output(self.micro.boot, 0)
+
+    def send_bootldr_cmd(self, cmd, checksum=True, sof=False):
+        console.log(f"Sending bootloader command: {tohex(cmd)}")
+        data = ([0x5a] if sof else []) + cmd + ([reduce(lambda x, y: x ^ y, cmd + [0xFF])] if checksum else [])
+        self.bus_xfer(data)
+    
+    def send_bootldr_ack(self):
+        self.bus_xfer([0x79])
+
+    def get_bootldr_ack(self):
+        self.bus_xfer([0x00])
+        while (res := self.bus_xfer([0x00])[0]) not in [0x79, 0x1F]:
+            time.sleep(0.1)
+        self.send_bootldr_ack()
+        return res == 0x79
+
+    def update_firmware(self, binary, base_address=0x08000000):
+        console.log("Updating firmware...")
+        self.reset_bootldr()
+        
+        self.gpio_output(self.micro.nss, 0)
+        self.send_bootldr_cmd([], checksum=False, sof=True)
+        self.get_bootldr_ack()
+
+        blocks = [binary[i:i+256] for i in range(0, len(binary), 256)]
+        blocks[-1] += b'\xFF' * (256 - len(blocks[-1]))
+        addresses = [base_address + i * 256 for i in range(len(blocks))]
+
+        for block, address in zip(blocks, addresses):
+            self.send_bootldr_cmd([0x31], sof=True)
+            self.get_bootldr_ack()
+            self.send_bootldr_cmd([address >> (24-i*8) & 0xFF for i in range(4)])
+            self.get_bootldr_ack()
+            self.send_bootldr_cmd([0xFF] + list(block))
+            self.get_bootldr_ack()
+    
+        self.send_bootldr_cmd([0x21], sof=True)
+        self.get_bootldr_ack()
+        self.send_bootldr_cmd([base_address >> (24-i*8) & 0xFF for i in range(4)])
+        self.get_bootldr_ack()
+
+        self.gpio_output(self.micro.nss, 1)
+
+
+        
+
